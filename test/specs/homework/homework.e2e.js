cd4873f3a57f17bec9e0f355b90ecbee9de05bf2
Joey-Hanna <joey.martin.de.lange@gmail.com>
2023-05-03 14:12:10 +0200

    homework lesson 2

--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -12,4 +12,41 @@ describe('Homework', async () => {
 
     });
 
+    it('find the best selectors for all the form elements', async () => {
+
+        await browser.reloadSession();
+        await browser.url('/prihlaseni');
+
+
+        /*
+        CSS Selectors: the first name and last name field selector
+         */
+        const idNameSelector = $('#name');
+        console.log(await idNameSelector.getHTML());
+
+        /*
+        CSS Selectors: the email field selector
+         */
+        const idEmailSelector = $('#email');
+        console.log(await idEmailSelector.getHTML());
+
+        /*
+        CSS Selectors: the password field selector 
+         */
+        const idPasswordSelector = $('#password');
+        console.log(await idPasswordSelector.getHTML());
+
+        /*
+        CSS Selectors: the password confirmation field selector 
+         */
+        const idPasswordConfirmSelector = $('#password-confirm');
+        console.log(await idPasswordConfirmSelector.getHTML());
+
+        /*
+        CSS Selectors: the registration button selector
+         */
+        const registrationButtonSelector = $('.btn-primary');
+        console.log(await registrationButtonSelector.getHTML());
+    });
+
 });
